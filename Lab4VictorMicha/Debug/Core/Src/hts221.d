808c3506a426e621970a4dc11fc58039949d6d74
Victor Micha <victor.micha@mail.mcgill.ca>
2023-11-07 17:10:24 -0500

    added files

---
+++ Lab4VictorMicha/Debug/Core/Src/hts221.d
@@ -0,0 +1,5 @@
+Core/Src/hts221.o: ../Core/Src/hts221.c ../Core/Inc/hts221.h \
+ ../Core/Inc/hsensor.h ../Core/Inc/tsensor.h
+../Core/Inc/hts221.h:
+../Core/Inc/hsensor.h:
+../Core/Inc/tsensor.h:
---
+++ Lab4VictorMicha/Debug/Core/Src/lps22hb.d
@@ -0,0 +1,5 @@
+Core/Src/lps22hb.o: ../Core/Src/lps22hb.c ../Core/Inc/lps22hb.h \
+ ../Core/Inc/psensor.h ../Core/Inc/tsensor.h
+../Core/Inc/lps22hb.h:
+../Core/Inc/psensor.h:
+../Core/Inc/tsensor.h:
